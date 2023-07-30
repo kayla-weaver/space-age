f4ec6eb977c07f03a53856ff3d832e23bb206212
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-29 20:37:37 -0700

    good fail on 5th test

--- _tests_/age.test.js
+++ _tests_/age.test.js
@@ -20,6 +20,11 @@ describe ("Person",() => {
     user.mars();
     expect(user.age).toBe(47);
 });
+it ("should return jupiter age at current value", () => {
+    const user = new Person(25);  
+    user.jupiter();
+    expect(user.age).toBe(296.5);
+});
 })
 
 
@@ -30,27 +35,3 @@ describe ("Person",() => {
 // Returns their age in Mars years. (A Mars year is 1.88 Earth years.)
 // Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)
 // Determines how many years have passed on each planet since a past birthday. For example, if a user is 56 and we want to calculate how many years have passed since their 43 birthday, we would find the following results:
-
-
-
-
-
-
-
-
-
-
-// describe('Age', () => {
-
-//     it('Should correctly create an age object', () => {
-//         let newAge = new Age();
-//         expect(newAge.mercury).toEqual("");
-//         expect(newAge.venus).toEqual("");
-//         expect(newAge.mars).toEqual("");
-//         expect(newAge.jupiter).toEqual("");
-//     });
-//     it('should return age on mercury', () => {
-//         const newAge = new Age();
-//         expect(newAge.returnLineArrayMercury(newAge.mercury)).toEqual(0);
-//     })
-// });
--- src/age.js
+++ src/age.js
@@ -31,31 +31,3 @@ export class Person {
 
 
 
-
-
-
-
-// export default class Age {
-//     constructor(mercury= "", venus="", mars="", jupiter="") {
-//         this.mercury = mercury;
-//         this.venus = venus;
-//         this.mars = mars;
-//         this.jupiter =  jupiter;
-//     }
-//     returnLineArrayMercury(age){
-//         let lineArray = [];
-//         if (age === ""){
-//             return lineArray*.24;
-//         }else  {
-//             return "meow";
-//         }
-//     }
-//     returnLineArrayVenus(age){
-//         let lineArray = [];
-//         if (age === ""){
-//             return lineArray*.62;
-//         } else {
-//             return "meow";
-//         }
-//     }
-// }
