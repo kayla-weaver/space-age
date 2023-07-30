1bcf2fa74d0f66940be358c3728f5228c99070ec
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-29 19:41:22 -0700

    redo of first test

--- _tests_/age.test.js
+++ _tests_/age.test.js
@@ -1,16 +1,37 @@
-import Age from "../src/age.js";
+import {Person} from "../src/age.js";
 
-describe('Age', () => {
+describe ("Person",() => {
+    it ("should create a person class with an age value") {
+        const user = new Person(25);
+        expect(user.age).toBe(25);
 
-    test('Should correctly create an age object', () => {
-        let newAge = new Age();
-        expect(newAge.mercury).toEqual("");
-        expect(newAge.venus).toEqual("");
-        expect(newAge.mars).toEqual("");
-        expect(newAge.jupiter).toEqual("");
-    });
-    test('should return age on mercury', () => {
-        const newAge = new Age();
-        expect(newAge.returnLineArrayMercury(newAge.mercury)).toEqual(0);
-    })
-});
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('Age', () => {
+
+//     it('Should correctly create an age object', () => {
+//         let newAge = new Age();
+//         expect(newAge.mercury).toEqual("");
+//         expect(newAge.venus).toEqual("");
+//         expect(newAge.mars).toEqual("");
+//         expect(newAge.jupiter).toEqual("");
+//     });
+//     it('should return age on mercury', () => {
+//         const newAge = new Age();
+//         expect(newAge.returnLineArrayMercury(newAge.mercury)).toEqual(0);
+//     })
+// });
--- src/age.js
+++ src/age.js
@@ -1,16 +1,44 @@
-export default class Age {
-    constructor(mercury= "", venus="", mars="", jupiter="") {
-        this.mercury = mercury;
-        this.venus = venus;
-        this.mars = mars;
-        this.jupiter =  jupiter;
-    }
-    returnLineArrayMercury(age){
-        let lineArray = [];
-        if (age === ""){
-            return lineArray*.24;
-        }else  {
-            return "meow";
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default class Age {
+//     constructor(mercury= "", venus="", mars="", jupiter="") {
+//         this.mercury = mercury;
+//         this.venus = venus;
+//         this.mars = mars;
+//         this.jupiter =  jupiter;
+//     }
+//     returnLineArrayMercury(age){
+//         let lineArray = [];
+//         if (age === ""){
+//             return lineArray*.24;
+//         }else  {
+//             return "meow";
+//         }
+//     }
+//     returnLineArrayVenus(age){
+//         let lineArray = [];
+//         if (age === ""){
+//             return lineArray*.62;
+//         } else {
+//             return "meow";
+//         }
+//     }
+// }
